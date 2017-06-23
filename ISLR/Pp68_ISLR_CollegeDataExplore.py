3d279754062fdd7c4e5cbc54ec43da4e1e97e136
Saptadeep <Saptadeep>
2017-06-23 06:28:52 +0530

    Routine commit

--- ISLR/Pp68_ISLR_CollegeDataExplore.py
+++ ISLR/Pp68_ISLR_CollegeDataExplore.py
@@ -1,13 +1,16 @@
+%config InlineBackend.figure_format = 'retina'
 #Pp68_ISLR_CollegeDataExplore
-
+from IPython.display import display, Image
 import pandas as pd
 import matplotlib as mplt
 import matplotlib.pyplot as plt
 
 from pandas.tools.plotting import scatter_matrix
 
-p_df = pd.read_csv("D:/Eclipse_Workspace/ISLR/CSVs/COllege.csv")
+p_df = pd.read_csv("College.csv")
 
 scatter_matrix(p_df.iloc[:,:10], alpha=0.2, figsize=(6, 6), diagonal='kde')
+plt.savefig('sctr23Jun.png')
+display(Image('sctr23Jun.png'))
 
 plt.show()
